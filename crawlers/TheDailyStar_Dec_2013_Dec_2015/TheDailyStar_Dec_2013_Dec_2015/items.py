4b851ce5a6b775d496aed5c2e488905a79df07dd
salmansaqib <a.salmansaqib@gmail.com>
2015-12-31 22:57:25 -0500

    The Daily Star crawler code for Dec 17, 2013 to Current Date

---
+++ crawlers/TheDailyStar_Dec_2013_Dec_2015/TheDailyStar_Dec_2013_Dec_2015/items.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class NewsVisItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+	headline = scrapy.Field()
+	link = scrapy.Field()
+	tag = scrapy.Field()	
+	date = scrapy.Field()
+	lastModified = scrapy.Field()	
+	reporter = scrapy.Field()
+	location = scrapy.Field()
+	newsBody = scrapy.Field()
+	
